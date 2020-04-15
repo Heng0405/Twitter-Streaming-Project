287220412f35c750ff38b4e9e762edb863ca9474
heng <fluty1116@gmail.com>
2020-04-15 19:04:57 +0200

    collected twittes by using #coronavirus

--- kafka-producer/src/main/java/KafkaTwitterProducer.java
+++ kafka-producer/src/main/java/KafkaTwitterProducer.java
@@ -5,12 +5,10 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import twitter4j.Status;
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
+import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
@@ -18,11 +16,12 @@ public class KafkaTwitterProducer {
 
 
     private static final Logger logger = LoggerFactory.getLogger(KafkaTwitterProducer.class);
+    public static final int NUM_TWEETS = 2000;
+    private final Object lock = new Object();
 
+    public static void main(String args[]) {
+    //    public List<Status> execute() {
 
-
-
-    public void start () {
             logger.info("Build Connection");
             ConfigurationBuilder cb = new ConfigurationBuilder();
             cb.setDebugEnabled(true)
@@ -31,26 +30,56 @@ public class KafkaTwitterProducer {
                     .setOAuthAccessToken(TwitterConfig.getInstance().getAccessToken())
                     .setOAuthAccessTokenSecret(TwitterConfig.getInstance().getAccessTokenSecret());
 
-            TwitterFactory tf = new TwitterFactory(cb.build());
-            Twitter twitter = tf.getInstance();
-            logger.info("Connection Réussie");
+        TwitterStream twitterStream = new TwitterStreamFactory(cb.build())
+                .getInstance(); // First you create the Stream
+        final List<Status> statuses = new ArrayList<Status>();
+        StatusListener listener = new StatusListener() {
 
-            Properties kafkaProperties = new Properties();
+
+            public void onException(Exception e) {
+                e.printStackTrace();
+            }
+            public void onStatus(Status status) {
+                statuses.add(status);
+                System.out.println("@ ---- "+status.getUser().getScreenName()+"-------"+"----------"+status.getText());
+            }
+            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+                System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
+            }
+
+            public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+                System.out.println("Got track limitation notice:" + numberOfLimitedStatuses);
+            }
+
+            public void onScrubGeo(long userId, long upToStatusId) {
+                System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
+            }
+            public void onStallWarning(StallWarning stallWarning) {
+            }
+        };
+        String[] keywords = {"#coronavirus"};
+        FilterQuery filterQuery = new FilterQuery();
+        filterQuery.track(keywords);
+        twitterStream.addListener(listener);
+        twitterStream.filter(filterQuery);
+
+        logger.info("Starting the twitter stream.");
+
+
+
+/*        Properties kafkaProperties = new Properties();
             kafkaProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:8082");
             kafkaProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
                     StringSerializer.class.getName());
             kafkaProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
                     StringSerializer.class.getName());
             kafkaProperties.put(ProducerConfig.ACKS_CONFIG, "all");
-            final Producer kafkaProducer = new KafkaProducer(kafkaProperties);
-
-
-
+            final Producer kafkaProducer = new KafkaProducer(kafkaProperties);*/
 
 
         }
 
 
-
     }
+
 
